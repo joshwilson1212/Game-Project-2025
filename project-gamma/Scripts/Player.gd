c7b4fd24b5fc58dfe80d73cbb9ee032c0fe5ea70
SavePleasantry <themrstealthy@gmail.com>
2025-05-16 10:15:01 -0500

    Added Dash and Change SPEED to a var (instead of const)
    added a Dash button (Space Key)
    and Change SPEED to a var (was  a const)

--- project-gamma/Scripts/Player.gd
+++ project-gamma/Scripts/Player.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 100
+var SPEED = 100
 const JUMP_VELOCITY = -150
+
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
@@ -10,8 +11,14 @@ var idle_direction = "Down Idel"
 
 func _physics_process(delta: float) -> void:
 	
-	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down",)
+	# Dash Code. Space key is the button for this
+	if Input.is_action_just_pressed("dash"):
+		$dashtimer.start()
+		SPEED *= 5
+		velocity = direction * SPEED
+		
 	if direction.x > 0:
 		idle_direction = "Right Idel"
 		animated_sprite.flip_h = false
@@ -29,7 +36,12 @@ func _physics_process(delta: float) -> void:
 	else:
 		animated_sprite.play(idle_direction)
 	
+
 	velocity = direction * SPEED
 	
 
 	move_and_slide()
+
+#resets to normal speed
+func _on_dashtimer_timeout() -> void:
+	SPEED = 100
