edb5998cd65d2304732f9edbbb5bb10137226d53
SavePleasantry <themrstealthy@gmail.com>
2025-05-16 12:21:00 -0500

    Added Sword
    placeholder animation and attack code

--- project-gamma/Scripts/Player.gd
+++ project-gamma/Scripts/Player.gd
@@ -4,15 +4,21 @@ extends CharacterBody2D
 var SPEED = 100
 const JUMP_VELOCITY = -150
 var is_ready: bool = true
-
+@export var attacking = false;
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-
+@onready var animation = $Sprites/Sword/AnimationPlayer
 var idle_direction = "Down Idel"
+
+#for sword 
+func _process(delta):
+	if Input.is_action_just_pressed("Attack"):
+		attack()
 
 func _physics_process(delta: float) -> void:
 	
 	
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down",)
+	
 	# Dash Code. Space key is the button for this
 	if Input.is_action_just_pressed("dash") and is_ready:
 		is_ready = false
@@ -47,3 +53,14 @@ func _physics_process(delta: float) -> void:
 func _on_dashtimer_timeout():
 	is_ready = true
 	SPEED = 100
+
+#Sword Code
+func attack():
+	attacking = true
+	animation.play("Attack")
+
+
+func _on_sword_hit_area_entered(area):
+	if area.is_in_group("hurtbox"):
+		area.take_damage()
+# end of sword code
