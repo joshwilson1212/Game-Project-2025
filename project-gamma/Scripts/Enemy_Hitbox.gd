5892d963be9ba128888d828d958f4c1aa938273b
joshwilson1212 <joshuawilson2001@gmail.com>
2025-05-30 05:06:24 -0500

    sword now rotates and faces properly and enemys now drop coins!

--- project-gamma/Scripts/Enemy_Hitbox.gd
+++ project-gamma/Scripts/Enemy_Hitbox.gd
@@ -1,6 +1,7 @@
 extends Area2D
 @onready var enemy_skeleton: Node2D = $"../.."
-
+@onready var game: Node2D = $"."
+const COIN = preload("res://coin.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -18,3 +19,12 @@ func _on_area_entered(area: Area2D):
 		enemy_skeleton.queue_free()
 	print("hit:{0}".format([area.name]))
 	pass # Replace with function body.
+
+#fucntion is called right before being "deleted" or freeed from the queue
+#will need to write some sort of function to determin drops
+#for now it will just drop something
+func _exit_tree() -> void:
+	var coin_instance = COIN.instantiate()
+	coin_instance.position = enemy_skeleton.global_position
+	get_tree().root.add_child(coin_instance)
+	print("SKELETON DEAD")
